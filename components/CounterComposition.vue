d29f0af3943c1b5c6860725e340ba55c3e477403
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-07-25 23:36:39 -0400

    Ref for typed in Typescript

--- components/CounterComposition.vue
+++ components/CounterComposition.vue
@@ -4,12 +4,12 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref } from 'vue';
+    import { defineComponent, Ref, ref } from 'vue'; //Tipado en TS
     export default defineComponent({
         name: 'CounterComposition',
         setup(){
-            let counter = ref(0); // ref: reactive value. Indica que el valor puede cambiar, es reactivo.
-            const addCounter = () => {
+            let counter:Ref = ref(0); // ref: reactive value. Indica que el valor puede cambiar, es reactivo.
+            const addCounter = ():void => {
                 counter.value++;
                 if(counter.value > 20){
                     counter.value = 0;
