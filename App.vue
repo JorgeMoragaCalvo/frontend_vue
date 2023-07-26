4ad116d59f1c622525ba30021b62d0a808fffc67
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-07-25 23:42:18 -0400

    caracteristicas setup

--- App.vue
+++ App.vue
@@ -4,7 +4,6 @@
 </template>
 
 <script lang="ts" setup>
-  import CounterOption from './components/CounterOption.vue';
   import CounterComposition from './components/CounterComposition.vue';
 </script>
 
--- components/CounterComposition.vue
+++ components/CounterComposition.vue
@@ -3,19 +3,13 @@
     <button @click="addCounter">Incrementar</button>
 </template>
 
-<script lang="ts">
-    import { defineComponent, Ref, ref } from 'vue'; //Tipado en TS
-    export default defineComponent({
-        name: 'CounterComposition',
-        setup(){
-            let counter:Ref = ref(0); // ref: reactive value. Indica que el valor puede cambiar, es reactivo.
-            const addCounter = ():void => {
-                counter.value++;
-                if(counter.value > 20){
-                    counter.value = 0;
-                }
+<script lang="ts" setup>
+    import { Ref, ref } from 'vue'; //Tipado en TS    
+    let counter:Ref = ref(0); // ref: reactive value. Indica que el valor puede cambiar, es reactivo.
+    const addCounter = ():void => {
+            counter.value++;
+            if(counter.value > 20){
+                counter.value = 0;
             }
-            return {counter, addCounter}
         }
-    })
 </script>
