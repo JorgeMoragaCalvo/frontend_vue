310f27eb11668dc98b1065340d700ac5a669fbf5
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-07-25 22:47:27 -0400

    learning components

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Listado de Post</h1>
+  <PostContainer/>
+</template>
+
+<script lang="ts" setup>
+  import PostContainer from './components/PostContainer.vue';
+</script>
+
+<style>
+  h1{
+    text-align: center;
+    color: green;
+  }
+</style>
---
+++ components/PostCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="post-card">
+        <h3>Titulo</h3>
+        <p>Netlify is a remote-first cloud computing company that offers a development platform that includes build, 
+            deploy, and serverless backend services for web applications and dynamic websites.</p>
+        <PostButton/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import PostButton from './PostButton.vue';
+</script>
+
+<style scoped>
+    .post-card{
+        border-radius: 15px;
+        padding: 10px;
+        margin: 5px;
+        border: 1px solid #ccc;
+    }
+</style>
+
