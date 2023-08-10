0f87615ff2a443df89d6aeabf3666e0afb9f4166
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2023-08-09 23:34:01 -0400

    v-model in checkbox and selects

--- components/ModelForm.vue
+++ components/ModelForm.vue
@@ -1,6 +1,12 @@
 <template>
     <form>
         <input type="text" v-model="inputText">
+        <input type="checkbox" v-model="agree">Aceptar términos y condiciones
+        <select v-model="country">
+            <option value="Spain">Spain</option>
+            <option value="Chile">Chile</option>
+            <option value="Uk">UK</option>
+        </select>
     </form>
     <h3>{{inputText}}</h3>
 </template>
@@ -8,6 +14,8 @@
 <script lang="ts" setup>
     import {Ref, ref } from 'vue';
     let inputText:Ref<string> = ref(''); // Ref<string> especifica el tipo de dato que se va a usar en Typescript
+    let agree = ref(true);
+    let country = ref('Chile');
 </script>
 
 <style scoped>
@@ -21,6 +29,20 @@
             setup(){
                 let inputText = ref('');
                 return {inputText};
+            }
+        });
+    </script>
+-->
+
+<!-- Option API
+    <script lang="ts">
+        import {ref, defineComponent } from 'vue';
+        export default defineComponent({
+            data(){
+                return {
+                    inputText: '',
+                    agree: true
+                };
             }
         });
     </script>
